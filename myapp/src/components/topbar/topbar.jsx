9add57f4eb94cd3860087ae022c1d991cb34cc04
Justin Jones <jus.jones@yahoo.com>
2024-08-12 10:58:57 -0500

    updating component

--- myapp/src/components/topbar/topbar.jsx
+++ myapp/src/components/topbar/topbar.jsx
@@ -1,13 +1,41 @@
 import '/Users/justinjones/Desktop/Tech/Continious Learning/social_app/myapp/src/components/topbar/topbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import Chat from '@mui/icons-material/Chat';
+import Notifications from '@mui/icons-material/Notifications';
 
 const topbar = () => {
   return (
-    <div className='topbarContainer'>topb
-        <div className="topbarLeft"></div>
-        <div className="topbarLeft"></div>
-        <div className="topbarLeft"></div>
-        <div className="topbarLeft"></div>
-    
+    <div className='topbarContainer'>
+        <div className="topbarLeft">
+          <span className="logo">Social Movie Review</span>
+        </div>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <input placeholder='Search for friend, post or video' className='searchInput' />
+            <SearchIcon />
+          </div>
+        </div>
+        <div className="topbarRight">
+            <div className="topbarLinks">
+                <span className='topbarLink'>Homepage</span>
+                <span className="topbarLink">Timeline</span>
+            </div>
+            <div className="topbarIcons">
+              <div className="topbarIconItem">
+                <PersonIcon />
+                <span className="topbarIconBadge">1</span>
+              </div>
+              <div className="topbarIconItem">
+                <Notifications />
+                <span className="topbarIconBadge">1</span>
+              </div>
+              <div className="topbarIconItem">
+                <Chat />
+                <span className="topbarIconBadge">1</span>
+              </div>               
+            </div>
+        </div>
     </div>
     
   )
