3c3efebd5055ce77bb16edf093a6e4a88dd30247
Justin Jones <jus.jones@yahoo.com>
2024-08-11 23:19:48 -0500

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './pages/home/Home';
+import PersonIcon from '@mui/icons-material/Person';
+
+
+function App() {
+  return (
+      <Home />
+  
+  );
+}
+
+export default App;
---
+++ myapp/src/components/topbar/topbar.jsx
@@ -0,0 +1,16 @@
+import '/Users/justinjones/Desktop/Tech/Continious Learning/social_app/myapp/src/components/topbar/topbar.css'
+
+const topbar = () => {
+  return (
+    <div className='topbarContainer'>topb
+        <div className="topbarLeft"></div>
+        <div className="topbarLeft"></div>
+        <div className="topbarLeft"></div>
+    
+    </div>
+    
+  )
+}
+
+export default topbar
+
