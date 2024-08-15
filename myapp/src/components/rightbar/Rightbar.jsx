c569908e51f228d968644da769c06df00fa5b7da
Justin Jones <jus.jones@yahoo.com>
2024-08-14 20:13:52 -0400

    updating components

--- myapp/src/components/rightbar/Rightbar.jsx
+++ myapp/src/components/rightbar/Rightbar.jsx
@@ -5,11 +5,23 @@ const Rightbar = () => {
     <div className='rightbar'> 
        <div className="rightbarWrapper">
            <div className="birthdayContainer">
-             <img src="" alt="" />
+             <img src="/assets/bday3.png" className='birthdayImg' alt="" />
              <span className='birthdayText'>
                <b>Pola Foster</b> and <b>3 other friends</b>have a birthday today
              </span>
            </div>
+           <img src='assets/nfl.jpeg' className='rightbarAd'/>
+           <h4 className='rightbarTitle'>Online</h4>
+           <ul className='rightbarFriendList'>
+             <li className="rightBarFriend">
+               <div className="rightbarProfileImgContainer">
+                 <img src="assets/profiles/2.jpg" alt=""
+                 className='rightbarProfileImg'
+                 />
+                 <span className='rightbarOnline'></span>
+               </div>
+             </li>
+           </ul>
        </div>
     </div>
   )
